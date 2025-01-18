52579b1be23737a7c887b2c8d7c93f9396b3f7c9
PaganMuffin <50595976+PaganMuffin@users.noreply.github.com>
2025-01-18 23:20:31 +0100

    fuck react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,23 @@ import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
+  output: "server",
+  adapter:cloudflare({
     platformProxy: {
       enabled: true
     }
   }),
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      // @ts-ignore
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
+  });
 
-  integrations: [tailwind(), react()]
-});
